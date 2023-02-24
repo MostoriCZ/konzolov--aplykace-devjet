ebd7bee6ab2ddf2cf55d3a9979920eae978c1a7d
MostoriCZ <jakubzeidel@gmail.com>
2023-02-24 11:47:08 +0100

    1.0.1

---
+++ vypisovac/Program.cs
@@ -0,0 +1,10 @@
+﻿for(int pocitadlo = 1; pocitadlo < 100; pocitadlo ++)
+{
+    if (pocitadlo != 40)
+    {
+        if (pocitadlo != 60)
+            Console.WriteLine(pocitadlo);
+    }
+}
+Console.WriteLine("end more");
+string cisilko = Console.ReadLine();
