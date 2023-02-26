c44b5197432ef9d2f6c6d603b3bc1958596359ce
MostoriCZ <jakubzeidel@gmail.com>
2023-02-26 21:35:46 +0100

    1.0.2

--- vypisovac/Program.cs
+++ vypisovac/Program.cs
@@ -1,4 +1,6 @@
-﻿for(int pocitadlo = 1; pocitadlo < 100; pocitadlo ++)
+﻿using System.Runtime.CompilerServices;
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+for (int pocitadlo = 1; pocitadlo < 100; pocitadlo ++)
 {
     if (pocitadlo != 40)
     {
@@ -6,6 +8,37 @@
             Console.WriteLine(pocitadlo);
     }
 }
-Console.WriteLine("end more");
-string cisilko = Console.ReadLine();
-//dd
+Console.WriteLine("KOnec");
+Console.WriteLine("Pokračovat dále? Y/N");
+string pismenko = Console.ReadLine();
+if (pismenko == "Y")
+{
+    // Dalsi ukol
+    Console.Clear();
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Zadej požadovanou šířku a výšku (např. 10x50)");
+    string input = Console.ReadLine();
+    string[] _size = input.Split("x");
+    int[] size = new int[_size.Length];
+    for (int i = 0; i < _size.Length; i++)
+    {
+        size[i] = int.Parse(_size[i]);
+    }
+    for (int i = 0; i < size[0]; i++)
+    {
+        for (int j = 0; j < size[1]; j++) Console.Write("* ");
+        Console.Write("\n");
+    }
+}
+Console.WriteLine("Pokračovat dále? Y/N");
+string pismenko2 = Console.ReadLine();
+if (pismenko2 == "Y")
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Magenta;
+//třetí program
+{
+    Console.WriteLine("napiš čísla které program následně vypíše ( odděl čárkou či tečkou)");
+    string input = Console.ReadLine() ?? "";
+    string[] array = input.Split(",");
+    Console.WriteLine($"Počet čísel je: {array.Length}");
+}
